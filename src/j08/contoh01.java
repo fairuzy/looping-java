48f486e23cd75731b8af624adc0426dafefab36a
Yusi Fayed <Yusi Fayed@MSI>
2018-08-02 11:14:12 +0700

    for, while, do while

---
+++ src/j08/contoh01.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j08;
+
+/**
+ *
+ * @author Yusi Fayed
+ */
+public class contoh01 {
+    public static void main(String[] args) {
+        
+        //Perulangan For
+        int x;
+        for(x = 1;x <= 5;x++);
+        {
+            System.out.println("SMK Telkom Malang");
+        }
+        //Perulanga While
+        int i = 1;
+        while(i <= 5)
+        {
+            System.out.println("SMK Telkom Malang");
+            i++;
+        }
+        
+        //Perulangan Do While
+        int j = 1;
+        do 
+        {
+            System.out.println("SMK Telkom Malang");
+            j++;
+        }
+        while(j <= 5);
+    }
+}
